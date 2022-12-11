3cb54d9f7520f13cc68cbddc91faa71c7e4dddfd
Agung Sundoro <muhagung2001@gmail.com>
2022-12-11 08:15:52 +0700

    Migrate git.go flag into flag.go

--- library/flag.go
+++ library/flag.go
@@ -6,14 +6,17 @@ import "flag"
 type Flag struct {
 	/** Mode */
 	Docker               *bool
+	Git                  *bool
 	DockerComposeRestart *bool
 	WPRefactor           *bool
 
 	/** Additional Mode */
 	Prune *bool
+	Push  *bool
 
 	/** Parameters */
-	ID *string
+	ID      *string
+	Message *string
 }
 
 /** Get Flag */
@@ -23,11 +26,14 @@ func GetFlag() Flag {
 	/** Mode */
 	flags.Docker = flag.Bool("docker", false, "Docker Mode")
 	flags.DockerComposeRestart = flag.Bool("docker-compose-restart", false, "Docker Compose Restart")
+	flags.Git = flag.Bool("git", false, "Git Mode")
 	flags.WPRefactor = flag.Bool("wp-refactor", false, "Refactor Library")
 
 	/** Parameters */
-	flags.Prune = flag.Bool("prune", false, "Prune (Docker Mode): Container")
 	flags.ID = flag.String("id", "", "Identifier (Docker Mode): Container")
+	flags.Prune = flag.Bool("prune", false, "Prune (Docker Mode): Container")
+	flags.Push = flag.Bool("push", false, "Push (Git Mode): Commit and Push")
+	flags.Message = flag.String("m", "", "Message (Git Mode): Commit Message")
 
 	return flags
 }
