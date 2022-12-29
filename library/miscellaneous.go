abf357b6ac1de03a4c4aa012a344be3f2de3fe0e
Agung Sundoro <muhagung2001@gmail.com>
2022-12-29 12:27:50 +0700

    feat: change searchandreplace command using native golang function

--- library/miscellaneous.go
+++ library/miscellaneous.go
@@ -2,15 +2,16 @@ package library
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	"runtime"
+	"path/filepath"
 	"strings"
 )
 
 /** Initiate Miscellaneous Function */
 func InitiateMiscellaneousFunction(flags Flag) {
 	/** Search and Replace */
-	if *flags.SearchandReplace && *flags.Path != "" && *flags.From != "" && *flags.To != "" {
+	if *flags.SearchandReplace && *flags.From != "" && *flags.To != "" {
 		SearchandReplace(*flags.Path, *flags.From, *flags.To)
 	}
 	/** Self Update */
@@ -23,39 +24,34 @@ func InitiateMiscellaneousFunction(flags Flag) {
 
 /**
 * Search and Replace in Directory or File
-* - Equivalent to : `LC_ALL=C find {path} -type f -name '*' -exec sed -i '' s/{from}/{to}/g {} +;`
-* - Equivalent to : `sed -i 's#{from}#{to}#g' {path}`
  */
 func SearchandReplace(path string, from string, to string) {
-	/** Define Delimiter */
-	DM := ""
-	if strings.Contains(from, "/") || strings.Contains(from, "/") {
-		DM = "#"
-	} else {
-		DM = "/"
+	if path == "" {
+		CurrentDirectory, _ := os.Getwd()
+		path = CurrentDirectory
 	}
 
-	/** Generate Shell Command based on Path Type (Directory or File) */
-	find := fmt.Sprintf("s%s%s%s%s%sg", DM, from, DM, to, DM)
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		panic(err)
-	}
-	if fileInfo.IsDir() {
-		os := runtime.GOOS
-		switch os {
-		case "darwin": /** MAC operating system */
-			find = fmt.Sprintf(`LC_ALL=C find %s -type f -name '*' -exec sed -i '' "%s" {} +;`, path, find)
-		case "linux": /** Linux */
-			find = fmt.Sprintf(`LC_ALL=C find %s -type f -name '*' -exec sed -i "%s" {} +;`, path, find)
-		default:
-			find = fmt.Sprintf(`LC_ALL=C find %s -type f -name '*' -exec sed -i "%s" {} +;`, path, find)
+	/** Search and Replace */
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
+		if !info.IsDir() {
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			newData := strings.Replace(string(data), from, to, -1)
+			err = ioutil.WriteFile(path, []byte(newData), 0644)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println("❌ ", err)
 	} else {
-		find = fmt.Sprintf("sed -i '' -e '%s' %s", find, path)
+		fmt.Println("✅ Success Search and Replace", from, "to", to, "in", path)
 	}
-	cmd := [...]string{"bash", "-c", find}
-	ExecCommand(cmd[:]...)
-
-	fmt.Println("✅ Success Search and Replace", from, "to", to, "in", path)
 }
