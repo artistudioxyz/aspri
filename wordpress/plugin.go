0de259f7a243e98461ed3dd6a022f2e094759988
Agung Sundoro <muhagung2001@gmail.com>
2024-04-05 10:17:10 +0700

    fix: autodetect filepath seperator for wordpress library

--- wordpress/plugin.go
+++ wordpress/plugin.go
@@ -3,6 +3,7 @@ package wordpress
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -11,18 +12,18 @@ func GetPluginInformation(path string) WPProject {
 	plugin := WPProject{}
 	plugin.Path.Directory = path
 
-	PathArray := strings.Split(path, "/")
+	PathArray := strings.Split(path, string(filepath.Separator))
 	plugin.Name = PathArray[len(PathArray)-1]
 	FileName := fmt.Sprintf("%s.php", PathArray[len(PathArray)-1])
-	plugin.Path.File = strings.Join(append(PathArray, FileName), "/")
+	plugin.Path.File = strings.Join(append(PathArray, FileName), string(filepath.Separator))
 
 	// Check if file exists
 	if _, err := os.Stat(plugin.Path.File); err != nil {
 		if os.IsNotExist(err) {
-			PathArray := strings.Split(path, "/")
+			PathArray := strings.Split(path, string(filepath.Separator))
 			plugin.Name = PathArray[len(PathArray)-2]
 			FileName := fmt.Sprintf("%s.php", PathArray[len(PathArray)-2])
-			plugin.Path.File = strings.Join(append(PathArray, FileName), "/")
+			plugin.Path.File = strings.Join(append(PathArray, FileName), string(filepath.Separator))
 		}
 	}
 
--- wordpress/theme.go
+++ wordpress/theme.go
@@ -1,6 +1,7 @@
 package wordpress
 
 import (
+	"path/filepath"
 	"strings"
 )
 
@@ -9,10 +10,10 @@ func GetThemeInformation(path string) WPProject {
 	theme := WPProject{}
 	theme.Path.Directory = path
 
-	PathArray := strings.Split(path, "/")
+	PathArray := strings.Split(path, string(filepath.Separator))
 	theme.Name = PathArray[len(PathArray)-1]
 	PathArray = append(PathArray, "style.css")
-	theme.Path.File = strings.Join(PathArray, "/")
+	theme.Path.File = strings.Join(PathArray, string(filepath.Separator))
 
 	return ReadCommentBlock(theme)
 }
